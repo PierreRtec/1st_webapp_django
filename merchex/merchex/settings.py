0adda1195ad5420e2a84ddd0d8c474a5076ef8ec
PierreRtec <pierre.rondeau9pro@gmail.com>
2021-11-17 08:27:53 +0100

    up settings

--- merchex/merchex/settings.py
+++ merchex/merchex/settings.py
@@ -19,6 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-!&vax)q=$eg3riiz1sf_m!--f2j0+(@#y(1fn4nor-p(rleboh'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
