ad89e589b61c0ba3509c4e704929bc0dec18deb1
PierreRtec <pierre.rondeau9pro@gmail.com>
2021-11-17 09:43:36 +0100

    up de nouvelles pages avec MVT

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def aboutus(request):
+    return HttpResponse('<h1>About us</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Liste des annonces</h1> <p>Annonces des articles :</p>')
+
+def contactus(request):
+    return HttpResponse('<h1>Contactez-nous ! </h2> Formulaire de contact</h2>')
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', views.hello),
+    path('about-us/', views.aboutus),    
+    path('listings/', views.listings),
+    path('contact-us/',views.contactus),
 ]
