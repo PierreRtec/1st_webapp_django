37b54c68afc79699c744677f89cf891876fe4e35
PierreRtec <pierre.rondeau9pro@gmail.com>
2021-12-03 08:30:55 +0100

    modif str + display admin date year genre

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -5,17 +5,22 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class Band(models.Model):
+    
+    def __str__(self):
+        return f'{self.name}'
 
     class Genre(models.TextChoices):
         HIP_HOP = 'HH'
         SYNTH_POP = 'SP'
         ALTERNATIVE_ROCK = 'AR'
+        SUPER_MUSIC = 'SM'
+        MEGA_COOL_STYLE = 'MCS'
 
     name = models.fields.CharField(max_length=100)
     genre = models.fields.CharField(choices=Genre.choices, max_length=5)
     biography = models.fields.CharField(max_length=1000)
     year_formed = models.fields.IntegerField(
-    validators=[MinValueValidator(2000), MaxValueValidator(2021)]
+    validators=[MinValueValidator(1000), MaxValueValidator(2021)]
     )
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
