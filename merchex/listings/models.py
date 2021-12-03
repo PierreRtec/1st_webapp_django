f06af663028204e859b902de5f8aae1f68fc7e5e
PierreRtec <pierre.rondeau9pro@gmail.com>
2021-12-03 09:06:12 +0100

    ajout erreur migration, remove avec nouvelle migration

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -8,7 +8,7 @@ class Band(models.Model):
         HIP_HOP = 'HH'
         SYNTH_POP = 'SP'
         ALTERNATIVE_ROCK = 'AR'
-
+    
     name = models.fields.CharField(max_length=100)
     genre = models.fields.CharField(choices=Genre.choices, max_length=5)
     biography = models.fields.CharField(max_length=1000)
